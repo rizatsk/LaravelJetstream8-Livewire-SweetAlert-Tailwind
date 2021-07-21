861a8049716aa1abea9dc85cf6fb5d6ec431a77e
rizatsk <rizatsakmir@gmail.com>
2021-07-21 12:03:46 +0700

    menambahkan file .docx dan menambahkan models

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+    // protected $table = 'nama_tabel'; untuk mendefinisikan model tersebut mewakili tabel apa
     protected $guarded = [];
     use HasFactory;
 }
