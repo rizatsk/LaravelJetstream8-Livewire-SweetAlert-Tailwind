11bd664b78a2ca1d938f33081031743141cfc169
sanggayasa <sanggayasa502@gmail.com>
2021-10-11 09:44:43 +0700

    front end tailwind

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         {{-- <div style="background-color: rgb(218, 218, 218)"> --}}
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Contact') }}
-            </h2>
-            <livewire:contact-index></livewire:contact-index>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Contact') }}
+        </h2>
+        <livewire:contact-index></livewire:contact-index>
         {{-- </div> --}}
     </x-slot>
-{{--    
+    {{--
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
